58b5ab02608be465d90b6636988944a33e0d68df
Ravi Singh <ravikrishnasingh@gmail.com>
2015-06-19 00:05:59 -0400

    terminal

--- public/terminal.js
+++ public/terminal.js
@@ -1,34 +1,62 @@
 var socket = io();
 
-socket.on("connect", function() {
-    console.log("connected");
-});
-
-socket.on("stdout", function(message) {
-    console.log(message.toString());
-});
-
-socket.on("stderr", function(message) {
-    console.log(message.toString());
-});
-
-socket.on("close", function(message) {
-    console.log("exit code " + message);
-});
-
-socket.on("disconnect", function() {
-    console.log("disconnected");
-});
-
-var setup = function() {
-    var input = document.getElementById("messager");
-    input.addEventListener("keypress", function(e) {
-        if(event.which == 13) {
-            event.preventDefault();
-            socket.emit("input", input.value);
-        }
-    });
+var format = function(message) {
+    return message
+        .replace(/\n/g, "<br/>")
+        .replace(/\s/g, "&nbsp;");
 };
 
-document.addEventListener("DOMContentLoaded", setup);
+var htmlEncode = function (value){
+    return $('<div/>').text(value).html();
+};
+ 
+var output = function(message) {
+    $("#stdout").append(format(htmlEncode(message)));
+    window.scrollTo(0, document.body.scrollHeight);
+};
+
+var send = function(value) {
+    socket.emit("input", value);
+};
+
+var isClearCommand = function(value) {
+    return value.toLowerCase() === "cls" || value.toLowerCase() === "clear";
+};
+
+var handleClearCommand = function(value) {
+    $("#stdout").html("");
+    send("");
+};
+
+$(function(){
+    $("#messager").on("keypress", function(e) {
+        if(e.which === 13) {
+            e.preventDefault();
+            var command = e.target.value;
+            if(isClearCommand(command)) handleClearCommand(command);
+            else send(command);
+            e.target.value = "";
+        }
+    });
+
+    socket.on("connect", function() {
+        $("#status").html("connected")
+            .removeClass("disconnected")
+            .addClass("connected");
+        $("#stdout").append("<br/><br/>");
+    });
+
+    socket.on("stdout", output);
+    socket.on("stderr", output);
+
+    socket.on("close", function(message) {
+        output("exit code " + message + "\n");
+    });
+
+    socket.on("disconnect", function() {
+        $("#status").html("disconnected")
+            .removeClass("connected")
+            .addClass("disconnected");
+    });
+});
 
--- server.js
+++ server.js
@@ -3,7 +3,21 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var spawn = require("child_process").spawn;
+
+var emit = false;
+var storeUntilConnect = "";
+
+var result = function(type) {
+    return function(data) {
+        if(emit) io.emit(type, data.toString());
+        else storeUntilConnect += data;
+    };
+};
+
 var cmd = spawn("cmd");
+cmd.stdout.on("data", result("stdout"));
+cmd.stderr.on("data", result("stderr"));
+cmd.on("close", result("close"));
 
 app.use(express.static(__dirname + "/public"));
 
@@ -18,17 +32,11 @@ http.listen(port, function() {
 });
 
 io.on("connection", function(socket) {
+    socket.emit("stdout", storeUntilConnect);
+    emit = true;
+
     socket.on("input", function(command) {
         cmd.stdin.write(command + "\n");
-        cmd.stdout.on("data", result("stdout"));
-        cmd.stderr.on("data", result("stderr"));
-        cmd.on("clost", result("close"));
     });
 });
-
-var result = function(type) {
-    return function(data) {
-        io.emit(type, data.toString());
-    };
-};
 
